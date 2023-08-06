e7b38e49222da825305f4ee92160a182c0618668
sim12389 <141345820+sim12389@users.noreply.github.com>
2023-08-06 10:55:13 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+const puppeteer = require('puppeteer');
+
+const URL_TO_OPEN = 'https://youtu.be/GHfpGqxF1a4'; 
+const DELAY_BEFORE_CLICK_MS = 10000;
+const DELAY_AFTER_CLICK_MS = 100000;
+
+(async () => {
+  try {
+    let isRunning = true;
+
+    while (isRunning) {
+      const browser = await puppeteer.launch({ headless: false }); // Mở trình duyệt Chrome
+      const page = await browser.newPage();
+
+      // Mở liên kết
+      await page.goto(URL_TO_OPEN);
+
+      // Chờ 10 giây
+      await page.waitForTimeout(DELAY_BEFORE_CLICK_MS);
+
+      // Tìm phần tử bạn muốn ấn và ấn vào nó
+      await page.waitForSelector('#ytd-player', { visible: true });
+      await page.click('#ytd-player');
+
+      // Chờ 100 giây
+      await page.waitForTimeout(DELAY_AFTER_CLICK_MS);
+
+      // Tắt trình duyệt
+      await browser.close();
+    }
+  } catch (error) {
+    console.error('Đã xảy ra lỗi:', error);
+  }
+})();
